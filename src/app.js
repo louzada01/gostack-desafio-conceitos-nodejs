8e4e1648bf3156dc5d456dc84f88b9969df77c24
Louzada <silvalouzada@gmail.com>
2020-04-09 22:31:21 -0300

    Criando repositorio com UUID

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,11 +11,15 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  return response.status(200).send({repositories});
+  return response.status(200).send(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  
+  const {title, url, techs} = request.body;
+  const repositorie = {id: uuid(), title, url, techs, likes: 0}
+  repositories.push(repositorie)
+
+  return response.status(200).send(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
