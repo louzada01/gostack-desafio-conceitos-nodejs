b891a155ce9910958a8ad423e6018abf98a11c24
Louzada <silvalouzada@gmail.com>
2020-04-09 23:11:39 -0300

    Feat: update repository

--- src/app.js
+++ src/app.js
@@ -15,15 +15,43 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const {title, url, techs} = request.body;
-  const repositorie = {id: uuid(), title, url, techs, likes: 0}
-  repositories.push(repositorie)
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
 
-  return response.status(200).send(repositorie);
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const findRepositoryIndex = repositories.findIndex(repository => 
+    repository.id === id  
+  );
+
+  if (findRepositoryIndex === -1) {
+    return response.status(400).json({ error: 'This repository does not exists.' });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[findRepositoryIndex].likes,
+  };
+
+  repositories[findRepositoryIndex] = repository
+
+  return response.status(200).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
